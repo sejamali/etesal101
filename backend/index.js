72d9ad415b65c9bac50c139c7190136c88088de8
Erfan Jamali <jamaliseyederfan@gmail.com>
2024-05-29 03:29:30 +0330

    swagger

--- backend/index.js
+++ backend/index.js
@@ -5,18 +5,11 @@ const swaggerUi = require('swagger-ui-express');
 const fs = require('fs');
 const jsyaml = require('js-yaml');
 
-const swaggerFiles = ['./endpoints/configTemplate/configTemplateSWG.yaml', './endpoints/packages/SWG.yaml'];
-const swaggerDocuments = [];
-swaggerFiles.forEach(swaggerFile =>
-{
-    const spec = fs.readFileSync(swaggerFile, 'utf8');
-    const swaggerDocument = jsyaml.load(spec);
-    swaggerDocuments.push(swaggerDocument);
-});
-const swaggerDocument = mergeSwaggerDocuments(swaggerDocuments);
-
 const packagesRouter = require('./endpoints/packages/crud');
+const packagesRouterSWG = jsyaml.load(fs.readFileSync('./endpoints/packages/packagesSWG.yaml', 'utf8'));
+
 const configTemplateRouter = require('./endpoints/configTemplate/crud');
+const configTemplateRouterSWG = jsyaml.load(fs.readFileSync('./endpoints/configTemplate/configTemplateSWG.yaml', 'utf8'));
 
 const app = express();
 
@@ -24,35 +17,16 @@ const port = 3000;
 
 app.use(express.json());
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Mount the routers
 app.use('/packages', packagesRouter);
+app.use('/packagesSWG', swaggerUi.serve, swaggerUi.setup(packagesRouterSWG));
+
 app.use('/configTemplate', configTemplateRouter);
+app.use('/configTemplateSWG', swaggerUi.serve, swaggerUi.setup(configTemplateRouterSWG));
 
 // Start the server
 app.listen(port, () =>
 {
     console.log(`Server is running at http://localhost:${port}`);
 });
-
-
-
-// Function to merge multiple Swagger documents
-function mergeSwaggerDocuments(documents)
-{
-    const mergedDocument = { ...documents[0] }; // Copy the first document
-
-    // Merge paths from all documents
-    for (let i = 1; i < documents.length; i++)
-    {
-        const document = documents[i];
-        if (document.paths)
-        {
-            mergedDocument.paths = { ...mergedDocument.paths, ...document.paths };
-        }
-    }
-
-
-    return mergedDocument;
-}
